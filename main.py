ebf9d9252a4c73bebe124f8b67d1d452ffc3547e
IdrisFallout <waithakasam2017@gmail.com>
2023-07-01 20:10:11 +0300

    add file explorer

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import os
-import socket
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
 
-def get_my_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(("8.8.8.8", 80))
-        ip_address = s.getsockname()[0]
-    except socket.error:
-        ip_address = "127.0.0.1"
-    finally:
-        s.close()
-    return ip_address
+class FileExplorer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("File Explorer")
+        self.root.geometry("500x300")
+
+        self.file_listbox = tk.Listbox(self.root, width=60, height=15)
+        self.file_listbox.pack(pady=10)
+
+        self.btn_frame = tk.Frame(self.root)
+        self.btn_frame.pack()
+
+        self.btn_open = tk.Button(self.btn_frame, text="Open", command=self.open_file)
+        self.btn_open.grid(row=0, column=0, padx=5)
+
+        self.btn_delete = tk.Button(self.btn_frame, text="Delete", command=self.delete_file)
+        self.btn_delete.grid(row=0, column=1, padx=5)
+
+        self.btn_refresh = tk.Button(self.btn_frame, text="Refresh", command=self.refresh_files)
+        self.btn_refresh.grid(row=0, column=2, padx=5)
+
+        self.refresh_files()
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            os.startfile(file_path)
+
+    def delete_file(self):
+        selected_index = self.file_listbox.curselection()
+        if selected_index:
+            file_name = self.file_listbox.get(selected_index)
+            confirm = messagebox.askyesno("Confirmation", f"Are you sure you want to delete {file_name}?")
+            if confirm:
+                file_path = os.path.join(os.getcwd(), file_name)
+                os.remove(file_path)
+                self.refresh_files()
+
+    def refresh_files(self):
+        self.file_listbox.delete(0, tk.END)
+        files = os.listdir()
+        for file_name in files:
+            self.file_listbox.insert(tk.END, file_name)
 
 
-IPAddress = '192.168.43.138'
-
-print(f"The file server ip is {IPAddress}")
+root = tk.Tk()
+app = FileExplorer(root)
+root.mainloop()
